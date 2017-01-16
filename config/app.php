38baf863a11829d788394c50ccdd7d9954046753
jatinrev <jatin@revinfotech.com>
2017-01-16 14:40:56 +0000

    completed code

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://laravel53.devserver.co.in'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
